37fd8de3deb4eb5c4772eb498880111903f8fb81
zhengjun <565061763@qq.com>
2019-07-11 22:12:38 +0800

    first commit

---
+++ lib/src/main/java/com/sd/lib/circularpgb/CircularProgressBar.java
@@ -0,0 +1,172 @@
+package com.sd.lib.circularpgb;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CircularProgressBar extends View
+{
+    private final Paint mPaint;
+
+    private int mProgress;
+    private int mMax = 100;
+
+    private int mNormalColor = Color.parseColor("#999999");
+    private int mProgressColor = Color.RED;
+
+    private int mProgressWidth;
+    private int mStartAngel = -90;
+
+    public CircularProgressBar(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setStyle(Paint.Style.STROKE);
+
+        mProgressWidth = (int) (getResources().getDisplayMetrics().density * 2);
+    }
+
+    /**
+     * 设置进度
+     *
+     * @param progress
+     */
+    public void setProgress(int progress)
+    {
+        if (progress < 0)
+            progress = 0;
+
+        if (progress > mMax)
+            progress = mMax;
+
+        if (mProgress != progress)
+        {
+            mProgress = progress;
+            invalidate();
+        }
+    }
+
+    /**
+     * 设置最大值
+     *
+     * @param max
+     */
+    public void setMax(int max)
+    {
+        if (max < 0)
+            max = 0;
+
+        if (mMax != max)
+        {
+            mMax = max;
+
+            if (mProgress > mMax)
+                setProgress(mMax);
+            else
+                invalidate();
+        }
+    }
+
+    /**
+     * 设置正常颜色值
+     *
+     * @param color
+     */
+    public void setNormalColor(int color)
+    {
+        if (mNormalColor != color)
+        {
+            mNormalColor = color;
+            invalidate();
+        }
+    }
+
+    /**
+     * 设置进度颜色值
+     *
+     * @param color
+     */
+    public void setProgressColor(int color)
+    {
+        if (mProgressColor != color)
+        {
+            mProgressColor = color;
+            invalidate();
+        }
+    }
+
+    /**
+     * 设置进度条宽度
+     *
+     * @param width
+     */
+    public void setProgressWidth(int width)
+    {
+        if (mProgressWidth != width)
+        {
+            mProgressWidth = width;
+            mPaint.setStrokeWidth(width);
+            invalidate();
+        }
+    }
+
+    /**
+     * 设置进度开始的的角度，默认顶部（-90度）
+     *
+     * @param angel
+     */
+    public void setStartAngel(int angel)
+    {
+        if (mStartAngel != angel)
+        {
+            mStartAngel = angel;
+            invalidate();
+        }
+    }
+
+    private float getProgressPercent()
+    {
+        if (mMax == 0)
+            return 0f;
+
+        return (float) mProgress / mMax;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+        super.onDraw(canvas);
+
+        final float width = getWidth() - getPaddingLeft() - getPaddingRight();
+        final float height = getHeight() - getPaddingTop() - getPaddingBottom();
+
+        final float x = getWidth() / 2 + (getPaddingLeft() - getPaddingRight()) / 2;
+        final float y = getHeight() / 2 + (getPaddingTop() - getPaddingBottom()) / 2;
+
+        final float radius = Math.min(width, height) / 2 - mProgressWidth / 2;
+        if (radius > 0)
+        {
+            final RectF rectF = newRectF(x, y, radius);
+
+            mPaint.setColor(mNormalColor);
+            canvas.drawArc(rectF, 0, 360, false, mPaint);
+
+            mPaint.setColor(mProgressColor);
+            canvas.drawArc(rectF, mStartAngel, 360 * getProgressPercent(), false, mPaint);
+        }
+    }
+
+    private static RectF newRectF(float x, float y, float radius)
+    {
+        final float left = x - radius;
+        final float right = x + radius;
+        final float top = y - radius;
+        final float bottom = y + radius;
+        return new RectF(left, top, right, bottom);
+    }
+}
