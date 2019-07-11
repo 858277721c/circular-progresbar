e7ef129ec32021b9b94b7e08d47c932e63302685
zhengjun <565061763@qq.com>
2019-07-11 22:39:55 +0800

    demo

--- app/src/main/java/com/sd/demo/circular_progresbar/MainActivity.java
+++ app/src/main/java/com/sd/demo/circular_progresbar/MainActivity.java
@@ -3,19 +3,13 @@ package com.sd.demo.circular_progresbar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.sd.lib.circularpgb.CircularProgressBar;
-
 public class MainActivity extends AppCompatActivity
 {
-    private CircularProgressBar mProgressBar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mProgressBar = findViewById(R.id.progressbar);
 
-        mProgressBar.setProgress(50);
     }
 }
